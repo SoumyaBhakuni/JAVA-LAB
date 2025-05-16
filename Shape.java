ff9305904c45a17dbfdd7d89245563e99cd22a50
SoumyaBhakuni <soumyabhakuni456@gmail.com>
2025-05-16 12:09:26 +0530

    Updated diretory structure

--- Shape.java
+++
@@ -1,8 +0,0 @@
-// Abstract class Shape
-abstract class Shape {
-    // Abstract methods
-    abstract void rectangleArea(double length, double breadth);
-    abstract void squareArea(double side);
-    abstract void circleArea(double radius);
-}
-
